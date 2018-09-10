0ceffb1999167a366ec85f60a64b5242384a5f85
Naoki Harima <k905672@kansai-u.ac.jp>
2018-09-10 11:20:13 +0900

    use http.StripPrefix with my handle

--- main.go
+++ main.go
@@ -7,33 +7,31 @@ import (
 	"strconv"
 )
 
-func main() {
-	http.HandleFunc("/fizzbuzz/", func(w http.ResponseWriter, r *http.Request) {
-		flag := false
-		var num int
-		if len(r.URL.Path) > 1 {
-			param := r.URL.Path[len("/fizzbuzz/"):]
-			val, err := strconv.Atoi(param)
-			if err != nil {
-				flag = false
-			} else {
-				flag = true
-				num = val
-			}
-		}
+type fbHandler string
 
-		switch {
-		case flag == false:
-			http.Error(w, "Invalid request.", http.StatusBadRequest)
-		case num%15 == 0:
-			fmt.Fprintf(w, "Fizz Buzz\n")
-		case num%3 == 0:
-			fmt.Fprintf(w, "Fizz\n")
-		case num%5 == 0:
-			fmt.Fprintf(w, "Buzz\n")
-		default:
-			fmt.Fprintf(w, "%d\n", num)
+func (fbh fbHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if len(r.URL.Path) > 0 {
+		num, err := strconv.Atoi(r.URL.Path)
+		if err == nil {
+			switch {
+			case num%15 == 0:
+				fmt.Fprintln(w, "Fizz Buzz")
+			case num%3 == 0:
+				fmt.Fprintln(w, "Fizz")
+			case num%5 == 0:
+				fmt.Fprintln(w, "Buzz")
+			default:
+				fmt.Fprintf(w, "%d\n", num)
+			}
+		} else {
+			http.Error(w, "Innumid request.", http.StatusBadRequest)
 		}
-	})
+	} else {
+		http.Error(w, "Innumid request.", http.StatusBadRequest)
+	}
+}
+func main() {
+	var fbh fbHandler
+	http.Handle("/fizzbuzz/", http.StripPrefix("/fizzbuzz/", fbh))
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
