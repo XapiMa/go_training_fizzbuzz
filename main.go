449850225bbd8a5962147d02ee32a809d57a6b69
Naoki Harima <k905672@kansai-u.ac.jp>
2018-09-10 14:07:07 +0900

    change error handling

--- main.go
+++ main.go
@@ -9,24 +9,20 @@ import (
 
 func main() {
 	fbf := func(w http.ResponseWriter, r *http.Request) {
-		if len(r.URL.Path) > 0 {
-			num, err := strconv.Atoi(r.URL.Path)
-			if err == nil {
-				switch {
-				case num%15 == 0:
-					fmt.Fprintln(w, "Fizz Buzz")
-				case num%3 == 0:
-					fmt.Fprintln(w, "Fizz")
-				case num%5 == 0:
-					fmt.Fprintln(w, "Buzz")
-				default:
-					fmt.Fprintf(w, "%d\n", num)
-				}
-			} else {
-				http.Error(w, "Innumid request.", http.StatusBadRequest)
-			}
-		} else {
+		num, err := strconv.Atoi(r.URL.Path)
+		if err != nil {
 			http.Error(w, "Innumid request.", http.StatusBadRequest)
+			return
+		}
+		switch {
+		case num%15 == 0:
+			fmt.Fprintln(w, "Fizz Buzz")
+		case num%3 == 0:
+			fmt.Fprintln(w, "Fizz")
+		case num%5 == 0:
+			fmt.Fprintln(w, "Buzz")
+		default:
+			fmt.Fprintln(w, num)
 		}
 	}
 	http.Handle("/fizzbuzz/", http.StripPrefix("/fizzbuzz/", http.HandlerFunc(fbf)))
